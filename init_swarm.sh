a6b3c87a343ed676b0a8cfe3e2cb4c7b162bf611
coder4 <lihy@fenbi.com>
2017-11-10 19:07:20 +0800

    ADD: tool node

--- init_swarm.sh
+++ init_swarm.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Leave all swarm nodes
-for i in 1 2 3;do
-    eval $(docker-machine env node-$i)
+for node in $(ls nodes);do
+    eval $(docker-machine env $node)
     docker swarm leave --force
 done
 
 # Const
 SWARM_MANAGER_NODE="node-1"
 SWARM_MANAGER_IP=$(docker-machine ip $SWARM_MANAGER_NODE)
+SWARM_SECOND_MANAGER_NODE="node-2"
+SWARM_TOOL_NODE="node-10"
 
 # Set node-1 as manager
 eval $(docker-machine env $SWARM_MANAGER_NODE)
@@ -19,22 +21,33 @@ docker swarm init \
 SWARM_MANAGER_TOKEN=$(docker swarm join-token -q manager)
 SWARM_WORKER_TOKEN=$(docker swarm join-token -q worker)
 
-# Join other 2 node
-for i in 2 3;do
-    eval $(docker-machine env node-$i)
+# Join other node
+for node in `ls nodes`;do
+
+    if [ x"$node" == x"$SWARM_MANAGER_NODE" ];then
+        continue
+    fi
+
+    # Join node as worker 
+    eval $(docker-machine env $node)
     SWARM_TOKEN=""
-    if [ $i -eq 2 ];then
+    if [ x"$node" == x"$SWARM_SECOND_MANAGER_NODE" ];then
         SWARM_TOKEN=$SWARM_MANAGER_TOKEN
     else
         SWARM_TOKEN=$SWARM_WORKER_TOKEN
     fi
     docker swarm join \
         --token $SWARM_TOKEN \
-        --advertise-addr $(docker-machine ip node-$i) \
+        --advertise-addr $(docker-machine ip $node) \
         $SWARM_MANAGER_IP:2377
 done
 
 echo ">> The swarm cluster is up and running"
+
+# tool label
+eval $(docker-machine env $SWARM_MANAGER_NODE)
+SWARM_NODE_ID_TOOL=docker node ls -f name=$SWARM_TOOL_NODE --format="{{.ID}}"
+docker node update --label-add tool
 
 # overlay network
 eval $(docker-machine env $SWARM_MANAGER_NODE)
--- run_openvpn_test.sh
+++ run_openvpn_test.sh
@@ -4,12 +4,13 @@
 NODE="node-1"
 NODE_DNS="node-1"
 VOLUME="/home/coder4/docker_data/openvpn"
-#dns_ip=$(docker-machine ip $NODE_DNS)
-dns_ip="8.8.8.8"
+dns_ip=$(docker-machine ip $NODE_DNS)
+#dns_ip="8.8.8.8"
 eval $(docker-machine env $NODE)
 # run server (should call init_open_vpn_test.sh before) 
 docker run \
     --network camp \
+    --dns $dns_ip \
     -d \
     -v $VOLUME:/etc/openvpn \
     -p 1194:1194/udp \
