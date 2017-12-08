d673ba4408d94047b2b2b9ac2f4ca15c5f269ac5
coder4 <lihy@fenbi.com>
2017-12-08 18:16:27 +0800

    FIX: Bug

--- revoke_openvpn_cert.sh
+++ revoke_openvpn_cert.sh
@@ -6,17 +6,11 @@ if [ x"$#" != x"1" ];then
 fi
 
 USERNAME="$1"
-CA_ROOT="/etc/openvpn/pki"
 
 TOOL_NODE="node-10"
 VOLUME="/home/coder4/docker_data/openvpn"
 eval $(docker-machine env $TOOL_NODE)
 
-# revoke client cert for username 
-docker run -v $VOLUME:/etc/openvpn --rm -it kylemanna/openvpn easyrsa revoke $USERNAME
+# revoke client cert by username
+docker run -v $VOLUME:/etc/openvpn --rm -it kylemanna/openvpn ovpn_revokeclient $USERNAME remove
 
-# need delete file
-FILE1="$CA_ROOT/issued/${USERNAME}.crt"
-FILE2="$CA_ROOT/private/${USERNAME}.key"
-FILE3="$CA_ROOT/reqs/${USERNAME}.req"
-docker run -v $VOLUME:/etc/openvpn --rm -it kylemanna/openvpn rm -rf $FILE1 $FILE2 $FILE3
